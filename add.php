6737fcf67a6d764ccd3c324db3a7d0b2632149ae
johnorlond <129138028+johnorlond@users.noreply.github.com>
2023-05-01 19:40:05 +0700

    Update add.php

--- add.php
+++ add.php
@@ -1,3 +1,19 @@
+<?php
+$servername = "mysql";
+$username = "root";
+$password = "password";
+$dbname = "redlock";
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$conn->select_db($dbname);
+
+$sql = "SELECT ID, Nama, Alamat, Jabatan FROM users";
+$result = $conn->query($sql);
 $sql = "SELECT COUNT(*) as count FROM users";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
